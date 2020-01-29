91d198c16d924aee6e484fb43c85a191aa4d2a3d
stephaniehenry <46508253+stephaniehenry@users.noreply.github.com>
2020-01-29 14:30:57 -0800

    Added Styling to the Player

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 console.log('OK');
 
 //Initialize Player//
-const player = new Plyr( '#player' );
+const player = new Plyr( '#player', {
+
+    controls: [ 'play', 'current-time', 'progress' ],
+  
+    keyboard: { focused: true, global: true },
+  
+    seekTime: 30,
+  
+});
